006ed2c3ad7a7166a97c5ed175030ab9d19abac7
Vahe Sevachyan <vscode13@gmail.com>
2024-02-26 20:51:27 -0800

    added async function to fetch weather data

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("linked");
+const apiKey = "293f148c81b5bec75b47839a4efae893";
+const apiURL =
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=paris";
+async function checkWeather() {
+  const response = await fetch(apiURL + `&appid=${apiKey}`);
+  const data = await response.json();
+  console.log(data);
+}
+checkWeather();
