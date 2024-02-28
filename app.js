d1b28943c257d9d21a2d5e539a88cf32d835c4ff
Vahe Sevachyan <vscode13@gmail.com>
2024-02-27 20:14:16 -0800

    updated query selectors

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const apiKey = "293f148c81b5bec75b47839a4efae893";
 const apiURL =
-  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=paris";
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=losangeles";
+const searchBtn = document.querySelector(".searchBtn");
+
 async function checkWeather() {
   const response = await fetch(apiURL + `&appid=${apiKey}`);
   const data = await response.json();
   console.log(data);
+  document.querySelector(".city").innerHTML = data.name;
+  document.querySelector(".temp").innerHTML = data.main.temp + "°c";
+  document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+  document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
 }
 checkWeather();
