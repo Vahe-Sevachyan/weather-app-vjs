369209021f8d37a7d7951beb8f8b7b773f1a412a
Vahe Sevachyan <vscode13@gmail.com>
2024-02-28 08:30:01 -0800

    updated checkWeather function

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 const apiKey = "293f148c81b5bec75b47839a4efae893";
 const apiURL =
-  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=losangeles";
-const searchBtn = document.querySelector(".searchBtn");
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
-async function checkWeather() {
-  const response = await fetch(apiURL + `&appid=${apiKey}`);
+const searchBtn = document.querySelector(".searchBtn");
+const searchField = document.querySelector(".searchField");
+async function checkWeather(city) {
+  const response = await fetch(apiURL + city + `&appid=${apiKey}`);
   const data = await response.json();
   console.log(data);
   document.querySelector(".city").innerHTML = data.name;
-  document.querySelector(".temp").innerHTML = data.main.temp + "°c";
+  document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
   document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
   document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
 }
+
 checkWeather();
+
+searchBtn.addEventListener("click", () => {
+  checkWeather(searchField.value);
+});
