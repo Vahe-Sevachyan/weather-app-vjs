949090486146cc1ee253268e8302fa3a5657c2fe
Vahe Sevachyan <vscode13@gmail.com>
2024-02-28 22:17:28 -0800

    updated checkWeather function to display forecast img

--- app.js
+++ app.js
@@ -4,18 +4,51 @@ const apiURL =
 
 const searchBtn = document.querySelector(".searchBtn");
 const searchField = document.querySelector(".searchField");
+const forecastImg = document.querySelector(".foreCastImg");
 async function checkWeather(city) {
   const response = await fetch(apiURL + city + `&appid=${apiKey}`);
   const data = await response.json();
+  // if (data) {
+  //   foreCastDisplay();
+  // }
   console.log(data);
   document.querySelector(".city").innerHTML = data.name;
   document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
   document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
   document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+  console.log(data.weather[0].main);
+
+  // foreCastImg(data.weather[0].main);
+
+  if (data.weather[0].main === "Clear") {
+    forecastImg.src = "images/clear.png";
+  } else if (data.weather[0].main === "Clouds") {
+    forecastImg.src = "images/clouds.png";
+  } else if (data.weather[0].main === "Drizzle") {
+    forecastImg.src = "images/drizzle.png";
+  } else if (data.weather[0].main === "Humidity") {
+    forecastImg.src = "images/humidity.png";
+  } else if (data.weather[0].main === "Mist") {
+    forecastImg.src = "images/mist.png";
+  } else if (data.weather[0].main === "Rain") {
+    forecastImg.src = "images/rain.png";
+  } else if (data.weather[0].main === "Snow") {
+    forecastImg.src = "images/snow.png";
+  } else if (data.weather[0].main === "Wind") {
+    forecastImg.src = "images/wind.png";
+  }
 }
 
-checkWeather();
+function foreCastDisplay() {
+  document.querySelector(".placeholder-message").style.display = "none";
+  document.querySelector(".temp").style.display = "init";
+  document.querySelector(".city").style.display = "init";
+  document.querySelector(".foreCastImg").style.display = "init";
+  document.querySelector(".col").style.display = "init";
+}
 
 searchBtn.addEventListener("click", () => {
   checkWeather(searchField.value);
 });
+
+function foreCastImg(weather) {}
